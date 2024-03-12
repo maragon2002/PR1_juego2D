805c12fc71b69a57ece4d34053bae0807116308b
heroniles <m.aragon2002@gmail.com>
2024-03-12 14:31:47 +0100

    Final clase 1

--- Assets/Scripts/MovPersonaje.cs
+++ Assets/Scripts/MovPersonaje.cs
@@ -1,18 +1,45 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Rendering;
 
 public class MovPersonaje : MonoBehaviour
 {
+
+    public float multiplicadorVelocidad = 1;
+    public float multiplicadorSalto=1;
+    Rigidbody2D rb;
+    SpriteRenderer sr;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        rb = this.GetComponent<Rigidbody2D>();
+       sr= this.GetComponent<SpriteRenderer>();
     }
 
     // Update is called once per frame
     void Update()
     {
+
+
+        //MOVIMIENTO
+
+        float mov = Input.GetAxis("Horizontal") * multiplicadorVelocidad * Time.deltaTime;
+        // transform.position=new Vector3(transform.position.x+mov,transform.position.y,transform.position.z);
+        transform.Translate(mov, 0, 0);
+        if(Input.GetKeyDown(KeyCode.A)){
+            sr.flipX=true;
         
+        }
+        if(Input.GetKeyDown(KeyCode.D)){
+            sr.flipX=false;
+        
+        }
+        
+        //SALTO
+     if(Input.GetKeyDown(KeyCode.Space)){
+        rb.AddForce(new Vector2(0,multiplicadorSalto),ForceMode2D.Impulse);
+     }
     }
 }
